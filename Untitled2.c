bd4ddc6a6563989b6fe47ce3295b2268c7290fb8
zorro54 <33323257+zorro54@users.noreply.github.com>
2022-06-01 16:21:44 +0200

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,21 @@
+Fonction rand
+Erreur de segmentation (core dumped) = Attend valeur
+ATOI : convertit chaine de caractères en entier
+
+time avant commande : donne le temps d'execution du programme
+Complexité : capacité à supporter la croissance du pb à traiter.
+Complexité linéaire (2*plus de données = 2*plus de temps)
+Complexité quadratique (2*plus de données = 4*plus de temps)
+Tant que c'est pas exponentiel, ça va
+
+
+Structures linéaires
+
+En C : tableaux
+Suite valeurs rangées les unes à côté des autres
+Taille fixe (donnée à la déclaration)
+Structure statique
+Homomorphe : tous les éléments d'un tableau sont de même type (ont la même taille), on fait des tableaux de float ou des tableaux d'entiers
+
+N éléments dans un tableau : de 0 à N-1
+Tableaux sont constants. Pour les copier, il faut faire cases par cases avec des boucles
